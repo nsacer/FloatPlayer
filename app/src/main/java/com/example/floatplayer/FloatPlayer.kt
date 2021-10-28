3c3f431869837b45cc1c7954b717af4e74625338
zhaopengfei <1250855973@qq.com>
2021-10-28 18:20:00 +0800

    FloatPlayer开发

--- app/src/main/java/com/example/floatplayer/FloatPlayer.kt
+++ app/src/main/java/com/example/floatplayer/FloatPlayer.kt
@@ -1,13 +1,19 @@
 package com.example.floatplayer
 
 import android.content.Context
+import android.graphics.PixelFormat
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.WindowManager
+import com.google.android.material.imageview.ShapeableImageView
 
 class FloatPlayer private constructor(context: Context) {
 
     companion object {
 
-        private lateinit var mLayoutParam: WindowManager.LayoutParams
+        private lateinit var layoutParam: WindowManager.LayoutParams
         @Volatile private var instance: FloatPlayer? = null
 
         @Synchronized
@@ -19,6 +25,30 @@ class FloatPlayer private constructor(context: Context) {
 
     fun init() {
 
-        mLayoutParam = WindowManager.LayoutParams()
+        layoutParam = WindowManager.LayoutParams()
+        layoutParam.width = WindowManager.LayoutParams.WRAP_CONTENT
+        layoutParam.height = WindowManager.LayoutParams.WRAP_CONTENT
+        //弹窗层级
+        layoutParam.type = WindowManager.LayoutParams.TYPE_APPLICATION
+        layoutParam.gravity = Gravity.START or Gravity.BOTTOM
+        //背景透明
+        layoutParam.format = PixelFormat.TRANSPARENT
+        //可以点击外部区域
+        layoutParam.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+        layoutParam.x =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24f,
+                FloatWindowApp.getAppContext().resources.displayMetrics)
+                .toInt()
+        layoutParam.y =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 80f,
+                FloatWindowApp.getAppContext().resources.displayMetrics)
+                .toInt()
+
+        val mViewFloat = LayoutInflater.from(FloatWindowApp.getAppContext())
+            .inflate(R.layout.float_player_view, null)
+        mViewFloat.findViewById<ShapeableImageView>(R.id.sivPlayerCover).setOnClickListener {
+            //TODO
+            Log.i("zhao", "icon点击")
+        }
     }
 }
