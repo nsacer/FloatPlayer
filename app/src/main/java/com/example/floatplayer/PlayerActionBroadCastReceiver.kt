b9c516c9509db6213fdef7b4c2590b4962760b2f
pary <1250855973@qq.com>
2021-11-04 21:36:52 +0800

    优化代码

--- app/src/main/java/com/example/floatplayer/PlayerActionBroadCastReceiver.kt
+++ app/src/main/java/com/example/floatplayer/PlayerActionBroadCastReceiver.kt
@@ -3,7 +3,6 @@ package com.example.floatplayer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 
 /**
  * 通知栏播放控制按钮通知播放器操作
@@ -19,11 +18,9 @@ class PlayerActionBroadCastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
             actionSwitch -> {
-                Log.i("zhao", "开始/暂停")
                 FloatPlayer.getInstance().playSwitch()
             }
             actionNext -> {
-                Log.i("zhao", "下一首")
                 FloatPlayer.getInstance().playNext()
             }
         }
